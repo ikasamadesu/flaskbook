9a331f3bd4ae8503bdbbdceda7883afd529b7e93
ikasama3 <ikasama3@gmail.com>
2022-08-14 08:41:12 +0900

    changed app route and templates engine

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def indec():
+def index():
     return "Hello, Flaskbook!"
+
+@app.route("/hello/<name>",
+    methods=["GET","POST"],
+    endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
